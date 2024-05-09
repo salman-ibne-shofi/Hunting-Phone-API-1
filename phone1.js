363d39b0fc6737223437887d80a37d9bcf452ad0
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-05-09 21:20:21 +0600

    display show details with nested object data

--- phone1.js
+++ phone1.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText = "13", isShowAll) => {
 	const res = await fetch(
 		`https://openapi.programming-hero.com/api/phones?search=${searchText}`
 	);
@@ -9,7 +9,7 @@ const loadPhone = async (searchText, isShowAll) => {
 };
 
 const displayPhones = (phones, isShowAll) => {
-	console.log(phones);
+	// console.log(phones);
 
 	const phoneContainer = document.getElementById("phone-container");
 	// clear phone container cards before adding cards
@@ -17,20 +17,20 @@ const displayPhones = (phones, isShowAll) => {
 
 	// display show all button if there are more than 12 phones
 	const showAllContainer = document.getElementById("show-all-container");
-	console.log(phones.length);
+	// console.log(phones.length);
 	if (phones.length > 12 && !isShowAll) {
 		showAllContainer.classList.remove("hidden");
 	} else {
 		showAllContainer.classList.add("hidden");
 	}
-	console.log("is show all", isShowAll);
+	// console.log("is show all", isShowAll);
 	// display only first 12 phones if not show All
 	if (!isShowAll) {
 		phones = phones.slice(0, 12);
 	}
 
 	phones.forEach((phone) => {
-		console.log(phone);
+		// console.log(phone);
 		// 2 creat a div
 		const phoneCard = document.createElement("div");
 		phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
@@ -58,13 +58,42 @@ const displayPhones = (phones, isShowAll) => {
 
 // handle show detail
 const handleShowDetail = async (id) => {
-	console.log("click show details", id);
+	// console.log("click show details", id);
 	// load single phone data
 	const res = await fetch(
 		`https://openapi.programming-hero.com/api/phone/${id}`
 	);
-    const data = await res.json();
-    console.log(data)
+	const data = await res.json();
+	console.log(data);
+	const phone = data.data;
+
+	showPhoneDetails(phone);
+};
+
+const showPhoneDetails = (phone) => {
+	// console.log(phone);
+	const phoneName = document.getElementById("show-detail-phone-name");
+	phoneName.innerText = phone.name;
+
+	const showDetailContainer = document.getElementById(
+		"show-detail-container"
+	);
+
+	showDetailContainer.innerHTML = `
+	<img src="${phone.image}" alt="" />
+    <p><span>Brand:</span>${phone.brand}</p>
+    <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span>Display Size:</span>${phone.mainFeatures.displaySize}</p>
+    <p><span>ChipSet:</span>${phone.mainFeatures.chipSet}</p>
+    <p><span>Sensors:</span>${phone.mainFeatures.sensors}</p>
+    <p><span>ReleaseDate:</span>${phone.releaseDate}</p>
+    <p><span>Slug:</span>${phone.slug}</p>
+    <p><span>GPS:</span>${phone?.others?.GPS}</p>
+
+	`;
+
+	// show the modal
+	show_details_modal.showModal();
 };
 
 // handle search button
@@ -98,4 +127,4 @@ const handleShowAll = () => {
 	handleSearch(true);
 };
 
-// loadPhone();
+loadPhone();
